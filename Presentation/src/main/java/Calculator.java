b46fcb62e56b737208c5d7b698b68d0878806ae0
ro-chest-47 <ro_chest_47@naver.com>
2019-05-03 01:29:31 +0900

    s

--- Presentation/src/main/java/Calculator.java
+++ Presentation/src/main/java/Calculator.java
@@ -1,4 +1,23 @@
 public class Calculator {
+
+    public static void main(String argv[]) {
+
+        int a = 10, b = 30;
+        int result_1, result_2, result_3, result_4;
+        Calcul cal = new Calcul();
+
+        result_1 = cal.add(a, b);
+        result_2 = cal.sub(a, b);
+        result_3 = cal.mul(a, b);
+        result_4 = cal.div(a, b);
+
+        System.out.println(result_1 + result_2 + result_3 + result_4);
+
+    }
+}
+
+class Calcul {
+
     public int add(int a, int b) {
         return a + b;
     }
@@ -15,3 +34,4 @@ public class Calculator {
         return a / b;
     }
 }
+
